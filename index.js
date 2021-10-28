8fe382888b428c5a013d3aeeccfe39b8dd9099fc
drethenrain <drethenrain@hotmail.com>
2021-10-28 20:11:21 -0300

    :tada: initing

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'no-shadow': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'class-methods-use-this': 'off',
+    'no-console': 'warn',
+  },
+};
