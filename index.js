b54f820b8b07bf6001024afc5460fb9835c95542
drethenrain <drethenrain@hotmail.com>
2021-10-28 21:53:26 -0300

    🔧 more rules

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 module.exports = {
   rules: {
+    'block-scoped-var': 'error',
+    'class-methods-use-this': 'off',
+    'comma-dangle': ['error', 'never'],
+    'eqeqeq': ['error', 'always', { null: 'ignore' }],
+    'no-console': 'warn',
+    'func-names': 'warn',
+    'indent': ['error', 2],
+    'no-var': 'error',
+    'no-void': 'error',
+    'no-useless-return': 'error',
+    'no-return-assign': ['error', 'always'],
+    'no-return-await': 'error',
+    'no-throw-literal': 'error',
+    'no-useless-catch': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-escape': 'error',
     'no-shadow': 'off',
     'no-unused-vars': 'off',
     'no-use-before-define': 'off',
-    'class-methods-use-this': 'off',
-    'no-console': 'warn',
-  },
+    'quotes': ['error', 'single'],
+    'require-await': 'off',
+    'semi': ['error', 'always'],
+    'yoda': 'error'
+  }
 };
